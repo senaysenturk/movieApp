3520aa9b783caf2ef83f3b5bbb583096bb61f771
Şenay ŞENTÜRK <senaysenturk@outlook.com>
2022-04-21 23:55:58 +0300

    App tasarımı ve componentlerin oluşturulması

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CategoryComponent } from './category/category.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movies/movie/movie.component';
+import { MoviesDetailsComponent } from './movies/movies-details/movies-details.component';
+import { FooterComponent } from './footer/footer.component';
 
-@NgModule({
+@NgModule({ //Componentler
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CategoryComponent,
+    MoviesComponent,
+    MovieComponent,
+    MoviesDetailsComponent,
+    FooterComponent
   ],
-  imports: [
+  imports: [ //Moduleler
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], //Servisler
+  bootstrap: [AppComponent] //Başlangıç componenti
 })
 export class AppModule { }
